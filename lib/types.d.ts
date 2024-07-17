bc67ae75ff01a982f944a8ac1940b8eeb318e7eb
Hadron67 <604700341@qq.com>
2024-07-18 00:38:28 +0800

    yet another refactor, expression nesting detection

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -1,8 +1,11 @@
+import type { Evaluator } from "./ordinals";
+
 export interface DisplayOptions {
     showSpecialVeblenFns: boolean;
 }
 
-export type Expression = number | CompoundExpression;
+export type PrimitiveExpression = number;
+export type Expression = PrimitiveExpression | CompoundExpression;
 
 export type DisplayNotation =
     | number
@@ -11,17 +14,29 @@ export type DisplayNotation =
     | {type: "superscript", expr: DisplayNotation[], superscript: DisplayNotation[]}
 ;
 
+export type LinkedListNode<T> = {prev: LinkedListNode<T> | null, data: T};
+/**
+ * We dont include the parent link in the expression nodes since expressions can have shared children, meaning there could be multiple parents.
+ * But certain algorithms do need parent links, that's what `TracedExpression` is used for.
+ */
+export type TracedExpression = {prev: TracedExpression | null, data: Expression};
+export type TracedExpressionWithIndex = {prev: TracedExpressionWithIndex, data: Expression, childIndex: number} | {prev: null, data: Expression};
+export type EvaluatorCallback = (ret: Expression, evaluator: Evaluator) => void;
+
+export interface ExpressionMessage {
+    apply(expr: Expression, evaluator: Evaluator): void;
+}
+
 export interface CompoundExpression {
     stringifyOne(): (string | Expression)[];
     getType(): string;
-    compareHead(other: Expression): Ordering;
+    compareHead(parent: TracedExpression | null, other: TracedExpression): Ordering;
     toDisplayNotationOne(todo: (Expression | ((stack: DisplayNotation[][]) => void))[], stack: DisplayNotation[][], opt: DisplayOptions): void;
-    evaluate(todo: [Expression, number][]): Expression;
-    evaluateUpValue(head: Expression, todo: [Expression, number][]): Expression;
+    evaluate2(parent: TracedExpression | null, cb: EvaluatorCallback, evaluator: Evaluator): boolean;
+    evaluateUpValue(head: TracedExpression, childIndex: number, cb: EvaluatorCallback, evaluator: Evaluator): boolean;
     getLength(): number;
     getChild(i: number): Expression;
-    setChild(i: number, expr: Expression): void;
-    appendChild(expr: Expression): void;
+    withChildren(children: Expression[]): CompoundExpression;
 
     foundamentalSequenceStep(n: number, stack: ((ord: Expression) => Expression)[]): [Expression, boolean];
     compareAsync(other: Expression, cb: (ret: Ordering) => void, exec: (run: () => void) => void): void;
