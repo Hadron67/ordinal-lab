286bfb23e1c4d76aa0187ba4a4521da3e9c3dc42
Hadron67 <604700341@qq.com>
2024-07-19 12:28:13 +0800

    overhaul: use summed types again, lambda expressions and fold nestings

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -6,15 +6,58 @@ export interface DisplayOptions {
 
 export type PrimitiveExpression = number;
 export type Expression = PrimitiveExpression | CompoundExpression;
-
-export type DisplayNotation =
-    | number
-    | {type: 'mi' | 'mo', value: string}
-    | {type: "subscript", expr: DisplayNotation[], subscript: DisplayNotation[]}
-    | {type: "superscript", expr: DisplayNotation[], superscript: DisplayNotation[]}
+export type CompoundExpression =
+    | PlusExpression
+    | TimesExpression
+    | PowerExpression
+    | LambdaExpression
+    | Omega
+    | Slot
 ;
 
-export type LinkedListNode<T> = {prev: LinkedListNode<T> | null, data: T};
+export interface ExpressionTypeMap {
+    'number': number;
+    'plus': PlusExpression;
+    'times': TimesExpression;
+    'power': PowerExpression;
+    'omega': Omega;
+    'lambda': LambdaExpression;
+    'slot': Slot;
+}
+
+export interface PlusExpression {
+    readonly type: 'plus';
+    readonly subexpressions: Expression[];
+}
+
+export interface TimesExpression {
+    readonly type: 'times';
+    readonly subexpressions: Expression[];
+}
+
+export interface PowerExpression {
+    readonly type: 'power';
+    readonly base: Expression;
+    readonly power: Expression;
+}
+
+export interface Omega {
+    readonly type: 'omega';
+}
+
+export interface LambdaExpression {
+    readonly type: 'lambda';
+    readonly args: number[];
+    readonly body: Expression;
+    readonly iterations: Expression;
+    readonly appliedTo?: Expression;
+}
+
+export interface Slot {
+    readonly type: 'slot';
+    readonly id: number;
+}
+
 /**
  * We dont include the parent link in the expression nodes since expressions can have shared children, meaning there could be multiple parents.
  * But certain algorithms do need parent links, that's what `TracedExpression` is used for.
@@ -23,43 +66,19 @@ export type TracedExpression = {prev: TracedExpression | null, data: Expression}
 export type TracedExpressionWithIndex = {prev: TracedExpressionWithIndex, data: Expression, childIndex: number} | {prev: null, data: Expression};
 export type EvaluatorCallback = (ret: Expression, evaluator: Evaluator) => void;
 
-export interface ExpressionMessage {
-    apply(expr: Expression, evaluator: Evaluator): void;
-}
-
-export interface CompoundExpression {
-    stringifyOne(): (string | Expression)[];
-    getType(): string;
-    compareHead(parent: TracedExpression | null, other: TracedExpression): Ordering;
-    toDisplayNotationOne(todo: (Expression | ((stack: DisplayNotation[][]) => void))[], stack: DisplayNotation[][], opt: DisplayOptions): void;
-    evaluate2(parent: TracedExpression | null, cb: EvaluatorCallback, evaluator: Evaluator): boolean;
-    evaluateUpValue(head: TracedExpression, childIndex: number, cb: EvaluatorCallback, evaluator: Evaluator): boolean;
-    getLength(): number;
-    getChild(i: number): Expression;
-    withChildren(children: Expression[]): CompoundExpression;
-
-    foundamentalSequenceStep(n: number, stack: ((ord: Expression) => Expression)[]): [Expression, boolean];
-    compareAsync(other: Expression, cb: (ret: Ordering) => void, exec: (run: () => void) => void): void;
-    maximizeAsync(cb: (ret: Expression | null) => void, exec: (run: MaximizerExecutor) => void): void;
-}
-
-export interface ExpressionVisitor {
-    visit(expr: Expression): void;
-}
-
 export type Ordering = 0 | 1 | -1;
 export type MaximizerExecutor = (n: number, opt: MaximizerOptions) => void;
 
 export interface VeblenFunction extends NestedArrayExpression {
-    type: "veblen";
+    readonly type: "veblen";
 }
 
 export type NestedArrayCoord = NestedArrayExpression | Expression;
 export type NestedArrayTerm = [NestedArrayCoord, Expression];
 
 export interface NestedArrayExpression {
-    positional: Expression[];
-    kw: [NestedArrayCoord, Expression][];
+    readonly positional: Expression[];
+    readonly kw: [NestedArrayCoord, Expression][];
 }
 
 export interface MaximizerOptions {
