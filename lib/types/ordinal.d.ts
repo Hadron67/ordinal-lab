327878a36ca4ec0e2bbd2515299bf3147175c3e1
Hadron67 <604700341@qq.com>
2024-12-31 16:53:54 +0800

    render BMS, more options in ordinal menu

--- lib/types/ordinal.d.ts
+++ lib/types/ordinal.d.ts
@@ -1,4 +1,6 @@
 import type { Evaluator } from "../ordinals";
+import type { OrdinalExpanderItem } from '../enumerator';
+import { LList } from './app';
 
 export interface DisplayOptions {
     showSpecialVeblenFns: boolean;
@@ -111,3 +113,39 @@ export type DisplayNotation =
 ;
 
 export type SpecialChar = 'psi' | 'omega' | 'Omega' | 'right-arrow' | 'alpha' | 'beta' | 'gamma';
+
+export interface Y0MontagneRow {
+    readonly data: number[];
+    readonly parentPtr: (number | null)[];
+}
+
+export interface Y0MontagneImageConfig {
+    elemGap: number;
+    linkCellHeight: number;
+    fontSize: number;
+    paddingLeftRight: number;
+}
+
+export interface RenderedY0Element extends LList<RenderedY0Element> {
+    readonly listItem: HTMLLIElement;
+    readonly data: number[];
+    readonly montagne: Y0MontagneRow[];
+}
+
+export interface OrdinalEnumeratorConfig<T> {
+    serialize(): any;
+    compare(a: T, b: T): number;
+    render(ord: T): Node;
+    stringify(a: T): string;
+    expander(a: T): (() => T | null) | null;
+    predecessor(a: T): T | null;
+    parse(str: string): T | null;
+    createElement(elem: OrdinalExpanderItem<T>): OrdinalEnumeratorElementManager<T>;
+}
+
+export interface OrdinalEnumeratorElementManager<T> {
+    render(ordinal: T, isCandidate: boolean): Node;
+    serialize(data: any): void;
+    deserialize(data: any): void;
+    createElement(elem: OrdinalExpanderItem<T>): OrdinalEnumeratorElementManager<T>;
+}
